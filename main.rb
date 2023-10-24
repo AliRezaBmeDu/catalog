cc5392d974a38a45b815d101b4a2cc805d71e4f9
AliReza <ali.reza.buet@gmail.com>
2023-10-24 19:05:05 +0600

    Change main.rb

--- main.rb
+++ main.rb
@@ -1,19 +1,23 @@
 require_relative 'item'
+require_relative 'app'
 
 TASKS = {
-  1 => :task1,
-  2 => :task2,
+  1 => :create_album,
+  2 => :list_album,
   3 => :task3,
   4 => :task4,
   5 => :task5,
   6 => :task6,
+  7 => :task7,
+  8 => :task8,
+  9 => :exit,
   default: :invalid_option
 }.freeze
 
 def display_main_menu
   puts "\nMain Menu: "
-  puts '1. Task 1'
-  puts '2. Task 2'
+  puts '1. Create Album'
+  puts '2. List Albums'
   puts '3. Task 3'
   puts '4. Task 4'
   puts '5. Task 5'
@@ -23,16 +27,17 @@ end
 
 def main
   puts "\n\nWelcome to the 'Catalog' app. Please make your choice and press Enter"
+  my_app = App.new
   loop do
     display_main_menu
     print 'input: '
     option = gets.chomp.to_i
-    if option == 7
+    if option == 9
       puts 'Exiting... Thank you for using the application'
       break
     end
-    TASKS[option] || TASKS[:default]
-    #   my_app.send(selection)
+    selection = TASKS[option] || TASKS[:default]
+    my_app.send(selection)
     puts 'Still to be implemented'
     puts "\n"
   end
