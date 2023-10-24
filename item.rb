d4ff0afebc2567a79cec89c8588b46e3254aa926
redwan <redtah2007@yahoo.com>
2023-10-24 00:55:11 -0700

    create the variables and initialize

--- item.rb
+++ item.rb
@@ -1,9 +1,14 @@
 # Class Item
 class Item
-  attr_accessor :var1, :var2
+  attr_accessor :id, :archived, :publish_date, :genre, :author, :source, :label
 
-  def initialize
-    @var1 = 'demo variable'
-    @var2 = 'demo for books'
+  def initialize(archived, publish_date)
+    @id = generate_id
+    @archived = archived
+    @publish_date = publish_date
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
   end
 end
