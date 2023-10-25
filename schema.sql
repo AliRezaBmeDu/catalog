6240a58e948eb46617169bce6bce51fed7116a3e
AliReza <ali.reza.buet@gmail.com>
2023-10-26 03:13:19 +0600

    Add sql commands for Album and Genre Table

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE item(
   label_id int, 
   author_id int,
   genre_id int NOT NULL,
-  CONSTRAINT fk_gene FOREIGN KEY (genre_id) REFERENCES genre(id),
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id),
   CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author(id),
   CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(id)
 );
@@ -27,5 +27,24 @@ CREATE TABLE Games(
     archived  boolean,
     author_id  int,
     CONSTRAINT fk_label FOREIGN KEY (author_id) REFERENCES Author(id),
+    PRIMARY KEY (id)
+);
+
+-- Create the Album table
+CREATE TABLE Album (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    artist VARCHAR(255),
+    publish_date DATE,
+    on_spotify BOOLEAN,
+    genre_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES Genre(id)
+);
+
+-- Create the Genre table
+CREATE TABLE Genre (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
